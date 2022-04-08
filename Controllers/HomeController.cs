98f22a68f3f6b95e4a615bd4688c0f50bcf5574c
feriel <ferielmaamer@yahoo.fr>
2022-04-08 18:37:28 -0400

    About

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace GBCSporting_X_TEAM.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Products()
         {
             var products = context.Products.OrderByDescending(x => x.ReleaseDate).ToList();
