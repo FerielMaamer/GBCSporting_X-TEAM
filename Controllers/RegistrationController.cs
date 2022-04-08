0d47338b35a7af1b457f75e22d77c90781923d0a
ROB <kaczurr@gmail.com>
2022-04-08 01:24:33 -0400

    Added commented out code.

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -38,19 +38,49 @@ namespace GBCSporting_X_TEAM.Controllers
 
 
 
+        //[HttpGet]
+        //public IActionResult Index(int id)
+        //{
+        //    ViewBag.message = null;
+        //    ViewBag.Products = context.Products.ToList();
+        //    var custID = id;
+        //    ViewBag.CustName = context.Customers.Where(x => x.CustomerId == custID).ToList();
+        //    IQueryable<Registration> query = context.Registrations;
+        //    query = query.Where(x => x.CustomerId == custID).Include(p => p.Product);
+        //    ViewBag.Registrations = query.ToList();
+        //    var registration = context.Registrations.Find(1, 2);
+        //    if ((query != null) && (!query.Any()))
+        //    {
+        //        ViewBag.message = "no results to show";
+        //        return View();
+        //    }
+        //    else
+        //    {
+        //        return View(registration);
+        //    }
+
+        //}
+
+
+
+
+        //[HttpPost]
+        //public IActionResult Register(Registration registration)
+        //{
+        //    registration.CustomerId = 1;
+        //        context.Registrations.Add(registration);
+        //    return RedirectToAction("Index", "Registration", new {id=1});
+            
+        //}
+
         [HttpPost]
         public IActionResult Register(Registration registration)
         {
-                registration.CustomerId = 1;
-                Registration model = new Registration()
-                {
-                    CustomerId = registration.CustomerId,
-                    ProductId = registration.ProductId,
-                };
-                context.Registrations.Update(registration);
-                context.SaveChanges();
-                return RedirectToAction("Index", "Registration");
-            
+            registration.CustomerId = 1;
+            context.Registrations.Add(registration);
+            context.SaveChanges();
+            //return RedirectToAction("Index", "Registration", new { id = 1 });
+            return RedirectToAction("Index", "Registration");
         }
 
 
