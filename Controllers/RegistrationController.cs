d99e02b1b87a1a0bf437bd31a1ce1f7e95675d58
feriel <ferielmaamer@yahoo.fr>
2022-04-08 03:55:00 -0400

    c1

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -20,6 +20,7 @@ namespace GBCSporting_X_TEAM.Controllers
             ViewBag.message = null;
             ViewBag.Products = context.Products.ToList();
             int custID = Int32.Parse(Request.Form["custId"]);
+            TempData["custID"] = custID;
             HttpContext.Session.SetInt32("custID", custID);
             ViewBag.CustName = context.Customers.Where(x => x.CustomerId == custID).ToList();
             IQueryable<Registration> query = context.Registrations;
@@ -38,26 +39,60 @@ namespace GBCSporting_X_TEAM.Controllers
 
         }
 
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            ViewBag.message = null;
+            ViewBag.Products = context.Products.ToList();
+            var custID = HttpContext.Session.GetInt32("custID");
+            ViewBag.CustName = context.Customers.Where(x => x.CustomerId == custID).ToList();
+            IQueryable<Registration> query = context.Registrations;
+            query = query.Where(x => x.CustomerId == custID).Include(p => p.Product);
+            ViewBag.Registrations = query.ToList();
+            var registration = context.Registrations.Find(custID,1);
+            if ((query != null) && (!query.Any()))
+            {
+                ViewBag.message = "no results to show";
+                return View();
+            }
+            else
+            {
+                return View(registration);
+            }
+
+        }
 
 
         [HttpPost]
         public IActionResult Register(Registration registration)
         {
-                registration.CustomerId = 1;
-                Registration model = new Registration()
+            
+            var id = TempData.Peek("custID");
+            //registration.CustomerId = Int32.Parse(id);
+
+               Registration model = new Registration()
                 {
                     CustomerId = registration.CustomerId,
                     ProductId = registration.ProductId,
                 };
-                if (context.Registrations.Find(1,registration.ProductId) == null)
+                if (context.Registrations.Find(id,registration.ProductId) == null)
                 {
                     context.Registrations.Add(registration);
                     context.SaveChanges();
-                    return RedirectToAction("Index", "Registration");
+                    return RedirectToAction("Index", "Registration", new { id = id });
                 }
                 
-                return RedirectToAction("Index", "Registration");
+                return RedirectToAction("Index", "Registration", new { id = id });
             
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int pid, int cid)
+        {
+            var registration = context.Registrations.Find(cid, pid);
+            context.Registrations.Remove(registration);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
 
