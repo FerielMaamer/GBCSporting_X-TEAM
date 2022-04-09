62567e93f51d854656b5bd17dc457f1f066c2d54
ROB <kaczurr@gmail.com>
2022-04-08 21:52:57 -0400

    Added more validation to the customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,23 +7,31 @@ namespace GBCSporting_X_TEAM.Models
         [Key]
         public int CustomerId { get; set; }
         [Required]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "Firstname requires between 1 and 51 characters")]
         public string FirstName { get; set; }
         [Required]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "Lastname requires between 1 and 51 characters")]
         public string LastName { get; set; }
         [Required]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "Address requires between 1 and 51 characters")]
         public string Address { get; set; }
         [Required]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "City requires between 1 and 51 characters")]
         public string City { get; set; }
         [Required]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "State requires between 1 and 51 characters")]
         public string State { get; set; }
         [Required]
+        [StringLength(21, MinimumLength = 1, ErrorMessage = "Post code requires between 1 and 21 characters")]
         public string PostalCode { get; set; }
 
         [Required]
         public int CountryId { get; set; }
         public Country? Country { get; set; }
-
+        [RegularExpression(@"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$", ErrorMessage = "Enter phone in format (999) 999-9999 ")]
         public string Phone { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         public string Slug => FirstName?.Replace(' ', '-').ToLower() + '-' + LastName?.Replace(' ', '-').ToLower();
 
